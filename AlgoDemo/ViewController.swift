38cc68aa9bc3b1c99c498535b3868badd5ca8e97
Shubham Kapoor <shubham.kapoor@mobileprogramming.net>
2018-09-12 10:21:53 +0530

    Initial Commit

---
+++ AlgoDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AlgoDemo
+//
+//  Created by Shubham Kapoor on 12/09/18.
+//  Copyright © 2018 Shubham Kapoor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
